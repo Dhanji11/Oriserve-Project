b5503d5dff2ded715714b1925ff5b52e6b597bef
Dhanji Verma <97548521+Dhanji11@users.noreply.github.com>
2024-09-07 17:51:15 +0530

    all files start to commit

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function displayResults (weather) {
 
 function dateBuilder (d) {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = ["Sundy", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
   let day = days[d.getDay()];
   let date = d.getDate();
